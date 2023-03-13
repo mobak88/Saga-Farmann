cd86e6647c9baf69927c7f0564acf263b7237d75
Renate <renate.hem@gmail.com>
2023-03-13 20:45:19 +0100

    fetch data

--- src/pages/crew/index.tsx
+++ src/pages/crew/index.tsx
@@ -1,30 +1,57 @@
 import React from "react";
 import CrewCard from "@/components/cards/crewCard/crewCard";
-import crewData from "@/components/cards/crewCard/data";
 import styles from "./crew.module.css";
-import Header from "@/components/header/Header";
-import Navbar from "@/components/navbar/navbar";
+import Header from "@/components/header/header";
+import Navbar from "@/components/navigation/navbar/navbar";
+import { GetStaticProps, NextPage } from "next";
 
-const Crew = () => {
+type Member = {
+  member_image: string;
+  member_name: string;
+  member_role: string;
+  member_description: string;
+};
+
+type CrewMember = {
+  id: number;
+  acf: {
+    member: Member[];
+  };
+};
+
+type Props = {
+  crewMembers: CrewMember[];
+};
+
+const Crew: NextPage<Props> = ({ crewMembers }) => {
   return (
     <>
       <Navbar />
       <Header header={"Crew"} />
-      <div className={styles["cards-container"]}>
-        {crewData.map((value) => {
-          return (
+
+      {crewMembers.map((crewMember) => (
+        <div key={crewMember.id} className={styles["card-container"]}>
+          {crewMember.acf.member.map((member, index) => (
             <CrewCard
-              key={value.id}
-              imageSrc={value.img}
-              name={value.name}
-              role={value.role}
-              about={value.about}
+              key={index}
+              member_image={member.member_image}
+              member_name={member.member_name}
+              member_role={member.member_role}
+              member_description={member.member_description}
             />
-          );
-        })}
-      </div>
+          ))}
+        </div>
+      ))}
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const res = await fetch(
+    "https://dev.sagafarmann.com/wp/wp-json/wp/v2/crew_members"
+  );
+  const crewMembers: CrewMember[] = await res.json();
+  return { props: { crewMembers } };
 };
 
 export default Crew;
