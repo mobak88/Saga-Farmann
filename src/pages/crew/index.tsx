994cc6c5498a754f47b677942247b5e42d44a7d5
Renate <renate.hem@gmail.com>
2023-03-14 16:37:00 +0100

    fetching by id

--- src/pages/crew/index.tsx
+++ src/pages/crew/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import CrewCard from "@/components/cards/crewCard/crewCard";
-import styles from "./crew.module.css";
-import Header from "@/components/header/header";
 import Navbar from "@/components/navigation/navbar/navbar";
-import { GetStaticProps, NextPage } from "next";
+import Header from "@/components/header/header";
+import Card from "../../components/cards/crewCard/crewCard";
+
+import styles from "./crew.module.css";
+import { GetStaticProps } from "next";
 
 type Member = {
   member_image: string;
@@ -13,45 +14,50 @@ type Member = {
 };
 
 type CrewMember = {
-  id: number;
-  acf: {
-    member: Member[];
-  };
+  title: { rendered: string };
+  acf: { member: Member[] };
 };
 
 type Props = {
-  crewMembers: CrewMember[];
+  crewMember: CrewMember;
 };
 
-const Crew: NextPage<Props> = ({ crewMembers }) => {
+const CrewMemberPage = ({ crewMember }: Props) => {
+  const { title, acf } = crewMember;
+
   return (
     <>
       <Navbar />
-      <Header header={"Crew"} />
-
-      {crewMembers.map((crewMember) => (
-        <div key={crewMember.id} className={styles["card-container"]}>
-          {crewMember.acf.member.map((member, index) => (
-            <CrewCard
+      <Header header={"Crew Page"} />
+      <h1>{title.rendered}</h1>
+      <div className={styles["card-container"]}>
+        <div>
+          {acf.member.map((member, index) => (
+            <Card
               key={index}
               member_image={member.member_image}
               member_name={member.member_name}
               member_role={member.member_role}
-              member_description={member.member_description}
+              member_description={member.member_name}
             />
           ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const res = await fetch(
-    "https://dev.sagafarmann.com/wp/wp-json/wp/v2/crew_members"
+    "https://dev.sagafarmann.com/wp/wp-json/wp/v2/crew_members/141"
   );
-  const crewMembers: CrewMember[] = await res.json();
-  return { props: { crewMembers } };
+  const crewMember: CrewMember = await res.json();
+
+  return {
+    props: {
+      crewMember,
+    },
+  };
 };
 
-export default Crew;
+export default CrewMemberPage;
