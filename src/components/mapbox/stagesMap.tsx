048e2185cb1ffe498a352ded9507f0e450746836
mobak88 <mostafa0401@ymail.com>
2023-03-14 11:41:00 +0100

    Removing unused imports

--- src/components/mapbox/stagesMap.tsx
+++ src/components/mapbox/stagesMap.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useState } from "react";
 import Map, { Marker, Source, Layer } from "react-map-gl";
 import { Feature, LineString } from "geojson";
 import "mapbox-gl/dist/mapbox-gl.css";
