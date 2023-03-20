5f4e0662ff62bcc13be2daa525a9b3a16d83d451
mobak88 <mostafa0401@ymail.com>
2023-03-20 13:41:37 +0100

    Adding map to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,28 @@ import HeadingOne from "@/components/typography/headings/headingOne";
 import Navbar from "@/components/navigation/navbar/navbar";
 import GridImagesAndText from "@/components/gridImagesAndText/gridImagesAndText";
 import styles from "./home.module.css";
+import StagesMap from "@/components/mapbox/stagesMap";
+import { StagesProps } from "@/components/mapbox/stagesMap";
 
-export default function Home() {
+interface SingleStageApiProps {
+  id: number;
+  title: { rendered: string };
+  acf: {
+    coordinates: {
+      long: string;
+      lat: string;
+    };
+    stage_number: number;
+    stage: [
+      {
+        stage_text_area: [{ stage_text: string }];
+      }
+    ];
+    current_destination: boolean;
+  };
+}
+
+export default function Home({ stages }: StagesProps) {
   return (
     <>
       <Head>
@@ -21,6 +41,7 @@ export default function Home() {
         <div className={styles["grid-wrapper"]}>
           <GridImagesAndText />
         </div>
+        <StagesMap stages={stages} />
         <LatestNews
           postHeading="Latest News and  posts"
           postText="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut."
@@ -29,4 +50,31 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://dev.sagafarmann.com/wp/wp-json/wp/v2/stages"
+  );
+  const stages = await res.json();
+
+  const newStages = stages.map((stage: SingleStageApiProps) => {
+    return {
+      id: stage.id,
+      title: stage.title.rendered,
+      coordinates: {
+        long: stage.acf.coordinates.long,
+        lat: stage.acf.coordinates.lat,
+      },
+      stage_number: stage.acf.stage_number,
+      stage_text_area: stage.acf.stage[0].stage_text_area,
+      current_destination: stage.acf.current_destination,
+    };
+  });
+
+  return {
+    props: {
+      stages: newStages,
+    },
+  };
 }
