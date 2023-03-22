4ecff8d030e9ee8e79a35aa328e6328917116a29
mobak88 <mostafa0401@ymail.com>
2023-03-22 12:29:46 +0100

    Adding waves on Home page

---
+++ src/components/waves/wavesSmallScreen/WaveRedBrownSmall.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "../Waves.module.css";
+
+const WaveRedBrownSmall = () => {
+  return (
+    <svg
+      className={styles["wave-small"]}
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="97.834"
+      viewBox="0 0 390.354 97.834"
+      preserveAspectRatio="none"
+      aria-hidden="true"
+    >
+      <path
+        id="NoPath_Kopi_2_"
+        data-name="NoPath – Kopi (2)"
+        d="M0,13.832H0L44.957,9.765H171.24L233.413.726l68.4-3.731L390.354.726v94.1H0Z"
+        transform="translate(0 3.005)"
+        fill="#bf625f"
+      />
+    </svg>
+  );
+};
+
+export default WaveRedBrownSmall;
