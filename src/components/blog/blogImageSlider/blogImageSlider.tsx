c687954f8448055f4b3467a0902cf793c443f329
hernok <hernok@hotmail.com>
2023-03-28 15:53:52 +0200

    adding swiper

--- src/components/blog/blogImageSlider/blogImageSlider.tsx
+++ src/components/blog/blogImageSlider/blogImageSlider.tsx
@@ -15,38 +15,65 @@ interface SliderProps {
 const BlogImageSlider = ({ images, alt }: SliderProps) => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   return (
-    <>
-      <Swiper
-        spaceBetween={10}
-        slidesPerView={3}
-        onSlideChange={() => console.log("Slider change")}
-        onSwiper={(swiper) => console.log(Swiper)}
-        breakpoints={{
-          640: {
-            slidesPerView: 1,
-          },
-          768: {
-            slidesPerView: 2,
-          },
-          1000: {
-            slidesPerView: 3,
-          },
-        }}
-        loop={true}
-      >
-        {images.map((image) => (
-          <SwiperSlide>
+    <div className={styles["image-swiper-container"]}>
+      {images.length > 1 ? (
+        <div>
+          <Swiper
+            className={styles["image-swiper"]}
+            spaceBetween={10}
+            navigation={true}
+            thumbs={{ swiper: thumbsSwiper }}
+            modules={[FreeMode, Navigation, Thumbs]}
+            loop={true}
+          >
+            {images.map((image) => (
+              <SwiperSlide>
+                <Image
+                  src={image.post_image}
+                  alt={alt}
+                  className={styles["swiper-focus-image"]}
+                  width={360}
+                  height={250}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <Swiper
+            onSwiper={() => setThumbsSwiper}
+            spaceBetween={10}
+            slidesPerView={images.length > 4 ? 4 : 3 ? 3 : 2 ? 2 : 1}
+            freeMode={true}
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="preview-swiper"
+          >
+            {images.map((image) => (
+              <SwiperSlide>
+                <Image
+                  src={image.post_image}
+                  alt={alt}
+                  className={styles["swiper-preview-image"]}
+                  width={360}
+                  height={250}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      ) : (
+        <>
+          {images.map((image) => (
             <Image
               src={image.post_image}
               alt={alt}
-              className={styles[""]}
+              className={styles["swiper-focus-image"]}
               width={360}
               height={250}
             />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
+          ))}
+        </>
+      )}
+    </div>
   );
 };
 export default BlogImageSlider;
