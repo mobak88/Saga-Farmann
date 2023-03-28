cde41ba29fea00c71a77c888b61a023512971d48
hernok <hernok@hotmail.com>
2023-03-28 12:23:51 +0200

    fixed mapping for new json

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -18,15 +18,10 @@ export interface Post extends Props {
   modified: string;
   title: { rendered: string };
   acf: {
-    post_first_section: PostFirstSection[];
+    post_first_section: PostFirstSection;
     post_second_section: PostSecondSection[];
     post_description: string;
   };
-}
-
-//posts.acf.post_first_section.post_images
-export interface PostImages extends PostFirstSection {
-  post_image: string;
 }
 
 //posts.acf.post_first_section
@@ -34,6 +29,11 @@ export interface PostFirstSection {
   post_images: PostImages[];
   post_first_heading: string;
   post_first_text: string;
+}
+
+//posts.acf.post_first_section.post_images
+export interface PostImages {
+  post_image: string;
 }
 
 //posts.acf.post_second_section
@@ -53,12 +53,18 @@ const BlogDetails = ({ post }: Props) => {
       <Header header={"Blog"} />
       <div className={styles["blog-id-container"]}>
         <div className={styles["grid-wrap"]}>
-          {post.acf.post_first_section[0].post_images.map((image, index) => (
-            <BlogImage image={image.post_image} alt={"Blog Image"} id={index} />
+          {post.acf.post_first_section.post_images.map((image, index) => (
+            <BlogImage
+              key={image.post_image + Math.random()}
+              image={image.post_image}
+              alt={"Blog Image"}
+              id={index}
+            />
           ))}
         </div>
         <div className={styles["blog-id-first-section"]}>
           <BlogInfo
+            key={Math.random()}
             id={post.id}
             modified={post.modified}
             title={post.title.rendered}
@@ -67,18 +73,22 @@ const BlogDetails = ({ post }: Props) => {
         </div>
         <div className={styles["blog-id-second-section"]}>
           {post.acf.post_second_section.map((secondSection) => (
-            <BlogSecondHeading
-              post_second_section_heading={
-                secondSection.post_second_section_heading
-              }
-            />
-            //  {secondSection.map(
-            // (text) => (
-            //   <BlogSecondText
-            //     post_second_section_text={text.post_second_section_text}
-            //   />
-            // )
-            //)}
+            <>
+              <BlogSecondHeading
+                key={secondSection.post_second_section_heading + Math.random()}
+                post_second_section_heading={
+                  secondSection.post_second_section_heading
+                }
+              />
+              {secondSection.post_second_section_texts.map((text) => (
+                <BlogSecondText
+                  key={
+                    text.post_second_section_text.slice(0, 20) + Math.random()
+                  }
+                  post_second_section_text={text.post_second_section_text}
+                />
+              ))}
+            </>
           ))}
         </div>
       </div>
