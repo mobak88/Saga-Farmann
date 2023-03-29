15f98f4c8b2f643ee11091169103fcce90a8f90f
Renate <renate.hem@gmail.com>
2023-03-29 15:27:52 +0200

    _on ts files

---
+++ src/components/latestNews/latestNewsSlider/_SliderData.ts
@@ -0,0 +1,72 @@
+const sliderData = [
+  {
+    id: 1,
+    imageSrc:
+      "https://dev.sagafarmann.com/wp/wp-content/uploads/2023/03/DJI_0858.jpg",
+    heading: "heading",
+    alt: "Captain",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+
+  {
+    id: 2,
+    imageSrc:
+      "https://dev.sagafarmann.com/wp/wp-content/uploads/2023/03/DJI_0858.jpg",
+    heading: "heading",
+    alt: "Captain",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    imageSrc:
+      "https://dev.sagafarmann.com/wp/wp-content/uploads/2023/03/DJI_0858.jpg",
+    heading: "heading",
+    alt: "Captain",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+
+  {
+    id: 4,
+    imageSrc:
+      "https://dev.sagafarmann.com/wp/wp-content/uploads/2023/03/DJI_0858.jpg",
+    heading: "heading",
+    alt: "Captain",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 5,
+    imageSrc:
+      "https://dev.sagafarmann.com/wp/wp-content/uploads/2023/03/DJI_0858.jpg",
+    heading: "heading",
+    alt: "Captain",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+
+  {
+    id: 6,
+    imageSrc:
+      "https://dev.sagafarmann.com/wp/wp-content/uploads/2023/03/DJI_0858.jpg",
+    heading: "heading",
+    alt: "Captain",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 7,
+    imageSrc:
+      "https://dev.sagafarmann.com/wp/wp-content/uploads/2023/03/DJI_0858.jpg",
+    heading: "heading",
+    alt: "Captain",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+
+  {
+    id: 8,
+    imageSrc:
+      "https://dev.sagafarmann.com/wp/wp-content/uploads/2023/03/DJI_0858.jpg",
+    heading: "heading",
+    alt: "Captain",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+
+export default sliderData;
