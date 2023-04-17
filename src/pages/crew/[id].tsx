257a20549a4dee132995412073440320b4f14d6e
mobak88 <mostafa0401@ymail.com>
2023-04-17 13:04:54 +0200

    Fixing responiveness in crew

--- src/pages/crew/[id].tsx
+++ src/pages/crew/[id].tsx
@@ -1,5 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from "next";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { ParsedUrlQuery } from "querystring";
 import styles from "./crew.module.css";
 import Header from "@/components/header/Header";
