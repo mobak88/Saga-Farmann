d8e4a7b2d85bbcd8b30a9920eae736c3d467c6d9
mobak88 <mostafa0401@ymail.com>
2023-04-18 23:27:22 +0200

    Testing

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -4,25 +4,23 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let slug = req.query.slug;
-  let path = "";
+  const { path, token } = req.query;
 
-  if (req.query.type === "post") {
-    path = "/blog/" + slug;
-  } else if (req.query.type === "page") {
-    path = "/" + slug;
-  }
-
-  if (req.query.secret !== process.env.REVALIDATION_SECRET) {
+  if ((token as string) !== process.env.REVALIDATION_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
+  } else if ((path as string).length === 0) {
+    return res.status(401).json({ message: "Path is required" });
   }
 
   try {
-    await res.revalidate(path);
-    return res.json({ revalidated: true, query: req.query });
-  } catch (error) {
-    return res
-      .status(500)
-      .send((error as { message?: string }).message ?? "Unknown error");
+    await res.revalidate(path as string);
+  } catch (err) {
+    return res.status(500).send("Error revalidating page");
   }
+
+  return res.status(200).json({
+    revalidated: true,
+    message: `Path ${path} revalidated successfully`,
+    req: req,
+  });
 }
