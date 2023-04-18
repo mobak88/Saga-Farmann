f457c53ca4d5aaab0962d771e314b551008ac2be
hernok <hernok@hotmail.com>
2023-04-18 19:00:14 +0200

    imports

--- src/pages/journey/index.tsx
+++ src/pages/journey/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "@/components/header/Header";
 import ParallaxMap from "@/components/parallax/map/ParallaxMap";
 import ParallaxText from "@/components/parallax/text/parallaxText";
-import { Parallax } from "swiper";
 
 const TheJourney = () => {
   return (
