2ec4f16cd917fcd20f92f2ee0c1a1755a19c02fe
hernok <hernok@hotmail.com>
2023-04-18 21:29:54 +0200

    started css parallax

--- src/components/parallax/text/ParallaxText.tsx
+++ src/components/parallax/text/ParallaxText.tsx
@@ -1,27 +1,22 @@
 import React from "react";
 import styles from "./ParallaxText.module.css";
 
-const ParallaxText = () => {
+interface TextProps {
+  heading: string;
+  text: string;
+  url: string;
+}
+
+const ParallaxText = ({ heading, text, url }: TextProps) => {
   return (
-    <div className={styles["parallax-text"]}>
-      <div className={styles["parallax-text1"]}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-        voluptatibus vitae, sapiente possimus soluta libero minus numquam
-        dolorum laudantium sed ullam corrupti velit, ad, maxime laboriosam modi.
-        Accusamus, nisi facere.
-      </div>
-      <div className={styles["parallax-text2"]}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-        voluptatibus vitae, sapiente possimus soluta libero minus numquam
-        dolorum laudantium sed ullam corrupti velit, ad, maxime laboriosam modi.
-        Accusamus, nisi facere.
-      </div>
-      <div className={styles["parallax-text3"]}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-        voluptatibus vitae, sapiente possimus soluta libero minus numquam
-        dolorum laudantium sed ullam corrupti velit, ad, maxime laboriosam modi.
-        Accusamus, nisi facere.
-      </div>
+    <div
+      className={styles["parallax-text"]}
+      style={{
+        backgroundImage: `url(${url})`,
+      }}
+    >
+      <div>{heading}</div>
+      <div>{text}</div>
     </div>
   );
 };
--- src/pages/journey/index.tsx
+++ src/pages/journey/index.tsx
@@ -1,22 +1,50 @@
 import React from "react";
 import Header from "@/components/header/Header";
-import ParallaxMap from "@/components/parallax/map/ParallaxMap";
 import ParallaxText from "@/components/parallax/text/parallaxText";
 import styles from "./journeyPage.module.css";
 
-const TheJourney = () => {
-  //   return (
-  //       <Header header={"The Journey"} />
-  //       <ParallaxText />
-  //       <ParallaxMap />
-  //
-  //   );
+interface ParallaxProps {
+  parallaxData: ParallaxDataProps[];
+}
 
-  //
+interface ParallaxDataProps {
+  heading: string;
+  text: string;
+  url: string;
+}
+
+const TheJourney = ({}: ParallaxProps) => {
+  const parallaxData: ParallaxDataProps[] = [
+    {
+      heading: "Heading",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi consectetur esse accusamus fugit deserunt in autem recusandae eaque ad. Dolores delectus aliquid autem voluptate soluta officiis nam repellendus laboriosam nesciunt.",
+      url: "https://dev.sagafarmann.com/wp/wp-content/uploads/website-core-images/DJI_0858-300x200.jpg",
+    },
+    {
+      heading: "Heading",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi consectetur esse accusamus fugit deserunt in autem recusandae eaque ad. Dolores delectus aliquid autem voluptate soluta officiis nam repellendus laboriosam nesciunt.",
+      url: "https://dev.sagafarmann.com/wp/wp-content/uploads/website-core-images/exp-2021-127.jpg",
+    },
+    {
+      heading: "Heading",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi consectetur esse accusamus fugit deserunt in autem recusandae eaque ad. Dolores delectus aliquid autem voluptate soluta officiis nam repellendus laboriosam nesciunt.",
+      url: "https://dev.sagafarmann.com/wp/wp-content/uploads/website-core-images/exp-2021-129.jpg",
+    },
+  ];
+
   return (
-    <div className={styles["parallax-text-container"]}>
-      <ParallaxText />
-    </div>
+    <>
+      <Header header={"The Journey"} />
+      <div className={styles["parallax-text-container"]}>
+        {parallaxData.map((data: ParallaxDataProps) => (
+          <ParallaxText
+            heading={data.heading}
+            text={data.text}
+            url={data.url}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
