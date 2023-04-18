05ac38f6250714eb924b8a04a128f45bdd3f55f8
hernok <hernok@hotmail.com>
2023-04-18 18:52:10 +0200

    journey

---
+++ src/pages/journey/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "@/components/header/Header";
+
+const TheJourney = () => {
+  return (
+    <>
+      <Header header={"The Journey"} />
+    </>
+  );
+};
+
+export default TheJourney;
