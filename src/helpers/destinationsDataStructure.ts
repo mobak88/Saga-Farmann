42d025aa81620324b3b95c9e83a594d2860b06d0
mobak88 <mostafa0401@ymail.com>
2023-04-19 18:08:58 +0200

    Adding correct links to destination, stage and blog for modal in map

--- src/helpers/destinationsDataStructure.ts
+++ src/helpers/destinationsDataStructure.ts
@@ -14,6 +14,12 @@ export const destinationsDataStructure = (
       number: destination.acf.destination_number,
       text_area: destination.acf.destination_text_fields[0].destination_text,
       next_year: destination.acf.next_year_destination,
+      blogs: Array.isArray(destination.acf.destination_blogs)
+        ? destination.acf.destination_blogs.map((blog) => {
+            return { blog: blog.destination_blog };
+          })
+        : false,
+      crew: destination.acf.destination_crew,
       image: destination.acf.destination_images[0].destination_image
         ? destination.acf.destination_images[0].destination_image.url
         : destination.acf.destination_images[0].destination_image,
