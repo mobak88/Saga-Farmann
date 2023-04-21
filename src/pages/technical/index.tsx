d6a1d3d4efe4f2615e101274417d4814c4344213
mobak88 <mostafa0401@ymail.com>
2023-04-21 20:27:05 +0200

    Finishing Technical

--- src/pages/technical/index.tsx
+++ src/pages/technical/index.tsx
@@ -8,8 +8,14 @@ import styles from "./Technical.module.css";
 import Image from "next/image";
 import { GetStaticProps } from "next";
 import API_ENDPOINTS from "@/endpoints/endpoints";
+import { WaveDarkHomeBottomPink } from "@/components/waves/wavesLargeScreen/WaveDarkHome";
+import { TechnicalInterface } from "./interfaces";
 
-const Technical = ({ pageData }: any) => {
+interface PageData {
+  pageData: TechnicalInterface;
+}
+
+const Technical = ({ pageData }: PageData) => {
   return (
     <>
       <Head>
@@ -18,14 +24,14 @@ const Technical = ({ pageData }: any) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header header="Technical" />
+      <Header header={pageData.title.rendered} />
       <DarkContainer>
         <div className={styles["section-container"]}>
           <div className={styles["image-wrapper"]}>
             <Image
               className={styles.image}
               src={pageData.acf.first_section_image}
-              alt="Technical image of saga farmann"
+              alt="Technical image of Klastadskipet seilforslag"
               height={600}
               width={600}
             />
@@ -33,15 +39,18 @@ const Technical = ({ pageData }: any) => {
           <div>
             <HeadingTwo>{pageData.acf.first_section_heading}</HeadingTwo>
             <div className={styles["paragraph-container"]}>
-              {pageData.acf.first_section_texts.map((text: any, i: number) => (
-                <ParagraphsBig key={i + Math.random()}>
-                  {text.first_section_text}
-                </ParagraphsBig>
-              ))}
+              {pageData.acf.first_section_texts.map(
+                (text: { first_section_text: string }, i: number) => (
+                  <ParagraphsBig key={i + Math.random()}>
+                    {text.first_section_text}
+                  </ParagraphsBig>
+                )
+              )}
             </div>
           </div>
         </div>
       </DarkContainer>
+      <WaveDarkHomeBottomPink />
       <div className={styles["second-section-wrapper"]}>
         <div
           className={`${styles["section-container"]} ${styles["second-section-container"]}`}
@@ -49,57 +58,121 @@ const Technical = ({ pageData }: any) => {
           <div className={styles["second-section-left-container"]}>
             <HeadingTwo>{pageData.acf.second_section_heading}</HeadingTwo>
             <div className={styles["paragraph-container"]}>
-              {pageData.acf.second_section_texts.map((text: any, i: number) => {
-                return i % 2 === 0 ? (
-                  <ParagraphsBig key={i + Math.random()}>
-                    {text.second_section_text}
-                  </ParagraphsBig>
-                ) : (
-                  <div
-                    className={styles["paragraph-with-img-container"]}
-                    key={i + Math.random()}
-                  >
-                    <ParagraphsBig>{text.second_section_text}</ParagraphsBig>
+              {pageData.acf.second_section_texts.map(
+                (text: { second_section_text: string }, i: number) => {
+                  return i % 2 === 0 ? (
+                    <ParagraphsBig key={i + Math.random()}>
+                      {text.second_section_text}
+                    </ParagraphsBig>
+                  ) : (
                     <div
-                      className={styles["second-section-motor-image-wrapper"]}
+                      className={styles["paragraph-with-img-container"]}
+                      key={i + Math.random()}
                     >
-                      <Image
-                        className={styles["second-section-motor-image"]}
-                        src={pageData.acf.second_section_image_three}
-                        alt={"alt"}
-                        height={900}
-                        width={600}
-                      />
+                      <ParagraphsBig>{text.second_section_text}</ParagraphsBig>
+                      <div
+                        className={styles["second-section-motor-image-wrapper"]}
+                      >
+                        <Image
+                          className={styles["second-section-motor-image"]}
+                          src={pageData.acf.second_section_image_three}
+                          alt={"Technical image of boat motor"}
+                          height={900}
+                          width={600}
+                        />
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                }
+              )}
             </div>
             <Image
               className={styles["second-section-horizontal-image"]}
               src={pageData.acf.second_section_image_one}
-              alt={"alt"}
+              alt={"Technical image of Klastad vre betelag"}
               height={900}
               width={600}
             />
             <Image
               className={styles["second-section-horizontal-image"]}
               src={pageData.acf.second_section_image_two}
-              alt={"alt"}
+              alt={"Technical image of Klastad nedre betelag"}
               height={300}
               width={800}
             />
           </div>
         </div>
       </div>
+      <div className={styles["wave-bottom-wrapper"]}>
+        <WaveDarkHomeBottomPink />
+      </div>
+      <DarkContainer>
+        <div className={styles["multiple-sections-container"]}>
+          <div className={styles["section-container"]}>
+            <div>
+              <HeadingTwo>{pageData.acf.third_section_heading}</HeadingTwo>
+              <div className={styles["paragraph-container"]}>
+                {pageData.acf.third_section_texts.map(
+                  (text: { third_section_text: string }, i: number) => (
+                    <ParagraphsBig key={i + Math.random()}>
+                      {text.third_section_text}
+                    </ParagraphsBig>
+                  )
+                )}
+              </div>
+            </div>
+            <div
+              className={`${styles["image-wrapper"]} ${styles["third-section-image-wrapper"]}`}
+            >
+              <Image
+                className={styles["third-section-image-horizontal"]}
+                src={pageData.acf.third_section_image_one}
+                alt="Technical image of Klastadskipet seilforslag"
+                height={600}
+                width={600}
+              />
+              <Image
+                className={styles["third-section-image-horizontal"]}
+                src={pageData.acf.third_section_image_two}
+                alt="Technical image of Klastadskipet seilforslag"
+                height={600}
+                width={600}
+              />
+            </div>
+          </div>
+          <div className={styles["section-container"]}>
+            <div className={styles["image-wrapper"]}>
+              <Image
+                className={styles.image}
+                src={pageData.acf.fourth_section_image}
+                alt="Technical image of Klastadskipet seilforslag"
+                height={600}
+                width={600}
+              />
+            </div>
+            <div>
+              <HeadingTwo>{pageData.acf.fourth_section_heading}</HeadingTwo>
+              <div className={styles["paragraph-container"]}>
+                {pageData.acf.fourth_section_texts.map(
+                  (text: { fourth_section_text: string }, i: number) => (
+                    <ParagraphsBig key={i + Math.random()}>
+                      {text.fourth_section_text}
+                    </ParagraphsBig>
+                  )
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </DarkContainer>
     </>
   );
 };
 
-export const getStaticProps: GetStaticProps<any> = async () => {
+export const getStaticProps: GetStaticProps<PageData> = async () => {
   const res = await fetch(API_ENDPOINTS.page(602));
 
-  const pageData: any = await res.json();
+  const pageData: TechnicalInterface = await res.json();
 
   return {
     props: {
