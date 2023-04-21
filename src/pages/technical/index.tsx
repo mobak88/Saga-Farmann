3b1b7c3cf62a740b82f0f57aadfb36e9c5090517
mobak88 <mostafa0401@ymail.com>
2023-04-21 20:32:08 +0200

    Removing interface file

--- src/pages/technical/index.tsx
+++ src/pages/technical/index.tsx
@@ -9,7 +9,27 @@ import Image from "next/image";
 import { GetStaticProps } from "next";
 import API_ENDPOINTS from "@/endpoints/endpoints";
 import { WaveDarkHomeBottomPink } from "@/components/waves/wavesLargeScreen/WaveDarkHome";
-import { TechnicalInterface } from "./interfaces";
+
+interface TechnicalInterface {
+  title: { rendered: string };
+  acf: {
+    first_section_heading: string;
+    first_section_texts: [{ first_section_text: string }];
+    first_section_image: string;
+    second_section_heading: string;
+    second_section_texts: [{ second_section_text: string }];
+    second_section_image_one: string;
+    second_section_image_two: string;
+    second_section_image_three: string;
+    third_section_heading: string;
+    third_section_texts: [{ third_section_text: string }];
+    third_section_image_one: string;
+    third_section_image_two: string;
+    fourth_section_heading: string;
+    fourth_section_texts: [{ fourth_section_text: string }];
+    fourth_section_image: string;
+  };
+}
 
 interface PageData {
   pageData: TechnicalInterface;
