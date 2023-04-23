f357675b0e377ddc946dbc2fc8e0d44b4f0665c4
Martine Evensen <martine.evensen@adstate.com>
2023-04-23 19:05:31 +0200

    added some dummydata, not working correctly

---
+++ src/pages/press/dummyData.ts
@@ -0,0 +1,37 @@
+export const archieve = [
+  {
+    id: 1,
+    image: "/dummyImages/destination.jpg",
+    alt: "Description of image 1",
+    caption: "Caption for image 1",
+    downloadUrl: "/downloads/destination.jpg",
+  },
+  {
+    id: 2,
+    image: "/dummyImages/BjornVik1.jpeg",
+    alt: "Description of image 2",
+    caption: "Caption for image 2",
+    downloadUrl: "/downloads/image1.jpg",
+  },
+  {
+    id: 3,
+    image: "/dummyImages/image-one.jpg",
+    alt: "Description of image 3",
+    caption: "Caption for image 3",
+    downloadUrl: "/downloads/image1.jpg",
+  },
+  {
+    id: 4,
+    image: "/dummyImages/image-two.png",
+    alt: "Description of image 4",
+    caption: "Caption for image 4",
+    downloadUrl: "/downloads/image1.jpg",
+  },
+  {
+    id: 5,
+    image: "/dummyImages/news-image.png",
+    alt: "Description of image 5",
+    caption: "Caption for image 5",
+    downloadUrl: "/downloads/image1.jpg",
+  },
+];
