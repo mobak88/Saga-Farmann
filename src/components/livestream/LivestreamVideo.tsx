26e8c5407286ba4aee3c82d1c0c6122ca331a93f
hernok <hernok@hotmail.com>
2023-04-25 14:12:23 +0200

    livestream test

--- src/components/livestream/LivestreamVideo.tsx
+++ src/components/livestream/LivestreamVideo.tsx
@@ -5,21 +5,22 @@ import styles from "./LivestreamVideo.module.css";
 const LivestreamVideo = () => {
   const [isOnline, setIsOnline] = useState<Boolean>(false);
 
-  const offlineDisplay: string =
-    "https://media.discordapp.net/attachments/1084295088739471451/1086015257715167362/Hernok_Vikings_on_a_voyage_through_europe_sleeping_on_the_ship._861ec5d8-5b26-4882-9b93-0b164f9dd4ca.png?width=1576&height=909";
-
   useEffect(() => {
-    getStatus();
+    fetch("/api/livestream/livestreamChecker")
+      .then((res) => res.json())
+      .then((data) => {
+        setIsOnline(data.isOnline);
+      });
   }, []);
-
-  async function getStatus() {
-    var resp = await fetch("/api/livestream/livestreamChecker");
-    const data = await resp.json();
-    setIsOnline(data.isOnline);
-  }
 
   return (
     <>
+      <iframe
+        width={"560px"}
+        height={"315px"}
+        className={styles["youtube-player"]}
+        src={`https://www.youtube.com/embed/live_stream?channel=${process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID}&autoplay=1&mute=1`}
+      ></iframe>
       <div className={styles["content-container"]}>
         <div className={styles["youtube-holder"]}>
           {isOnline ? (
@@ -39,4 +40,5 @@ const LivestreamVideo = () => {
     </>
   );
 };
+
 export default LivestreamVideo;
