f339ef55969b8ae8ebd59783c3aff8e9f1e99b9c
mobak88 <mostafa0401@ymail.com>
2023-04-25 14:12:39 +0200

    Making nav links shared to avoid changing links muiltiple places

--- src/components/footer/FooterLinks.tsx
+++ src/components/footer/FooterLinks.tsx
@@ -1,35 +1,25 @@
 import Link from "next/link";
 import styles from "./Footer.module.css";
 import { useCurrentCrewId } from "@/hooks/useCurrentCrewId";
+import { links } from "../navigation/hamburger/links";
 
 const FooterLinks = () => {
   const crewId = useCurrentCrewId();
 
-  const links = [
-    { href: "/", label: "Home" },
-    { href: `/crew_members/${crewId}`, label: "Crew" },
-    { href: "/posts", label: "Blog" },
-    { href: "/destinations", label: "Destinations" },
-    { href: "/livestream", label: "Livestream" },
-    { href: "/sponsors", label: "Sponsors" },
-    { href: "/donate", label: "Donate" },
-    { href: "/aboutus", label: "About Us" },
-    { href: "/the_ship", label: "The Ship" },
-    { href: "/pressarchive", label: "Press" },
-  ];
+  const pageLinks = links(crewId as string);
 
   return (
     <>
       <div className={styles["container-links"]}>
         <ul className={styles["wrapper-links"]}>
-          {links.slice(0, 5).map(({ href, label }) => (
+          {pageLinks.slice(0, 5).map(({ href, label }) => (
             <Link key={href} className={styles["footer-links"]} href={href}>
               <>{label}</>
             </Link>
           ))}
         </ul>
         <ul className={styles["wrapper-links"]}>
-          {links.slice(5, 10).map(({ href, label }) => (
+          {pageLinks.slice(5, 10).map(({ href, label }) => (
             <Link key={href} className={styles["footer-links"]} href={href}>
               <>{label}</>
             </Link>
