20bc3842c1cee4ab007611bcc97447581f9bbd81
mobak88 <mostafa0401@ymail.com>
2023-05-23 12:16:05 +0200

    Testing slider buttons

--- src/components/latestNews/latestNewsSlider/LatestNewsSlider.tsx
+++ src/components/latestNews/latestNewsSlider/LatestNewsSlider.tsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useRef } from "react";
 import SliderCard from "@/components/cards/sliderCard/SliderCard";
 import { PostsProps } from "../latestNewsInterfaces";
 
+import SwiperCore from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+
 import styles from "./LatestNewsSlider.module.css";
 
 const LatestNewsSlider = ({ posts }: PostsProps) => {
+  const swiperNavPrevRef = useRef<HTMLDivElement>(null);
+  const swiperNavNextRef = useRef<HTMLDivElement>(null);
+
+  const onBeforeInit = (Swiper: SwiperCore): void => {
+    if (typeof Swiper.params.navigation !== "boolean") {
+      const navigation = Swiper.params.navigation;
+      if (navigation !== undefined) {
+        navigation.prevEl = swiperNavPrevRef.current;
+        navigation.nextEl = swiperNavNextRef.current;
+      }
+    }
+  };
+
   return (
     <div className={styles["swiper-container"]}>
+      <div className={styles["swiper-nav-prev"]} ref={swiperNavPrevRef}>
+        <BsFillArrowLeftCircleFill className={styles["arrow-icon"]} size={50} />
+      </div>
+      <div className={styles["swiper-nav-next"]} ref={swiperNavNextRef}>
+        <BsFillArrowRightCircleFill
+          className={styles["arrow-icon"]}
+          size={50}
+        />
+      </div>
       <Swiper
-        navigation
+        navigation={{
+          prevEl: swiperNavPrevRef.current,
+          nextEl: swiperNavNextRef.current,
+        }}
+        onBeforeInit={onBeforeInit}
+        simulateTouch={false}
         className={styles["news-swiper"]}
         modules={[Navigation]}
         spaceBetween={20}
